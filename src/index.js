ff8736d4eb19f3d048f42fcf3d0a2aabe010570b
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2020-03-23 18:22:50 -0300

    express app handle error

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { config } from 'dotenv'
+config()
 import express from 'express'
 import bodyParser from 'body-parser'
 import '@babel/polyfill'
@@ -6,10 +8,12 @@ import cors from 'cors'
 import errorhandler from 'errorhandler'
 import morgan from 'morgan'
 import chalk from 'chalk'
-
+import { AuthMiddleware } from './middleware'
 import SwaggerJSDoc from 'swagger-jsdoc'
 import swaggerUi from 'swagger-ui-express'
 import { options } from './config/swagger'
+import { users } from './routers'
+
 let swaggerSpec = SwaggerJSDoc(options)
 
 var app = express()
@@ -26,6 +30,25 @@ if (env.NODE_ENV === 'development') {
 // Import and add Routers here. 
 const BASE_PATH = '/v1'
 
+app.use(AuthMiddleware)
+
+app.use(`${BASE_PATH}/users`, users)
+
+
+
+// Custom Handle Error
+app.use((err, req, res, next) => {
+    if (err && err.error && err.error.isJoi) {
+        // we had a joi error, let's return a custom 400 json response
+        res.status(400).json({
+            type: err.type, // it could be 'headers', 'body', 'query' or 'params'
+            message: err.error.toString()
+        })
+    } else {
+        // pass on to another error handler
+        next(err)
+    }
+})
 
 swaggerSpec.servers = [{ url: `${process.env.BASE_URL || 'https://node-template.mybluemix.net'}${BASE_PATH}` }]
 app.use(`${BASE_PATH}/api-docs`, swaggerUi.serve, swaggerUi.setup(swaggerSpec))
---
+++ src/models/storage/index.js
@@ -0,0 +1,4 @@
+import _user from './user'
+
+
+export const User = _user
