657e143cb206038dd2f7a204babc0122f3e694a8
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2020-03-23 18:20:51 -0300

    Storage Config added

---
+++ src/config/storage/index.js
@@ -0,0 +1,15 @@
+import { config } from 'dotenv'
+config()
+import fs from 'fs'
+import path from 'path'
+
+export const DATABASE_URL = process.env.DATABASE_URL
+
+
+
+
+export const DATABASE_MODELS = {
+    USER: 'User'
+}
+
+export const ca = fs.readFileSync(path.join(__dirname, 'ca.pem'), 'utf8')
