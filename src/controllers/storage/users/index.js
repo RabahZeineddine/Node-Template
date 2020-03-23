22c889d24c6ee238d2d8f6844853bffb5e2bb9e3
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2020-03-23 18:20:12 -0300

    User Storage Controller added

---
+++ src/controllers/storage/users/index.js
@@ -0,0 +1,68 @@
+import StorageController from '..'
+
+import { User } from '../../../models/storage'
+
+export default class UsersStorageController extends StorageController {
+
+
+    async findByEmail(email, select = '') {
+        return new Promise(async (resolve, reject) => {
+            try {
+                if (email) {
+                    let user = await User.findOne({ 'email': email }).select(select)
+                    resolve(user)
+                } else {
+                    throw { code: 400 }
+                }
+            } catch (error) {
+                console.error(error)
+                reject({ code: error.code || 500 })
+            }
+        })
+    }
+
+    async create(user) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                if (user) {
+                    let userSchema = new User(user.toDatabase())
+                    let newUser = await userSchema.save()
+                    resolve(newUser)
+                } else {
+                    throw { code: 400 }
+                }
+            } catch (error) {
+                reject({ code: error.code || 500 })
+            }
+        })
+    }
+
+    async findAll(select = '-_id -__v') {
+        return new Promise(async (resolve, reject) => {
+            try {
+                let users = await User.find({}).select(select)
+                resolve(users)
+            } catch (error) {
+                console.error(error)
+                reject({ code: error.code || 500 })
+            }
+        })
+    }
+
+    async findOneAndUpdate(user, select = '') {
+        return new Promise(async (resolve, reject) => {
+            try {
+                user.modifiedDate = new Date()
+                user = await User.findOneAndUpdate({ '_id': user._id }, user).select(select)
+                if (user) {
+                    resolve(user)
+                } else {
+                    throw { code: 500 }
+                }
+            } catch (error) {
+                console.error(error)
+                reject({ code: error.code || 500 })
+            }
+        })
+    }
+}
